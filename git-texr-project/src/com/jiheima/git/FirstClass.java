657adf42d8d1c8eba500de66720d9020ecab0fd6
ChiWah <463154196@qq.com>
2019-12-02 17:54:19 +0800

    把本地工程添加到git

---
+++ git-texr-project/src/com/jiheima/git/FirstClass.java
@@ -0,0 +1,19 @@
+package com.jiheima.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git projext");
+        System.out.println("hello git projext");
+
+        System.out.println("hello git projext");
+        System.out.println("hello git projext");
+        System.out.println("hello git projext");
+        System.out.println("hello git projext");
+        System.out.println("hello git projext");
+
+
+
+
+
+    }
+}
